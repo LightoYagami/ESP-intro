e63a9945fb8f9dc60963c1ef6c4ff9b111f8eafe
unknown <mjangid2349@gmail.com>
2023-01-25 21:33:39 +0530

    touch and led sync

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,19 @@
 
 void setup(){
   Serial.begin(115200);
+  pinMode(2,OUTPUT);
+  Serial.print("Coming from setup");
 }
 
 void loop(){
     Serial.println(touchRead(4));
+    Serial.print("Coming from loop");
     delay(300);
+
+    if(touchRead(4) > 40){
+        digitalWrite(2, LOW);
+    }
+    else{
+        digitalWrite(2, HIGH);
+    }
 }
