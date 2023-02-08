9bd3c0155f103dbe2a23a0e84f2cfab28c34b4c0
unknown <mjangid2349@gmail.com>
2023-02-08 19:44:38 +0530

    blink ESP-32 Diode

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,15 @@
 #include <Arduino.h>
 
-int val  =  0;
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-   Serial.begin(9600);
+pinMode(LED_BUILTIN OUTPUT);
+ digitalWrite(LED_BUILTIN,HIGH);
+ delay(1000);
 }
 
-void loop() {  
-   val = hallRead();
-   if(val >= 80) {
-     digitalWrite(LED_BUILTIN, HIGH);
-     delay(1000);
-     }
-   
-   else {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(2000);
-
-   //Serial.println(val);
-   delay(200);
-  }
+void loop() {
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(2000);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(1000);g
 }
 
